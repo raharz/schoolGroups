15d1bb0f9a0b2c2207d8c41f18c703153d5dca56
raha <raharezaaeii@gmail.com>
2024-07-15 16:01:17 +0330

    update main.js students

--- main.js
+++ main.js
@@ -1,12 +1,48 @@
 let school = {
   classA: {
-    student1: {},
-    student2: {},
-    student3: {},
+    student1: {
+      firstName: "aaa",
+      lastName: "aaaa",
+      phoneNumber: "111",
+      address: "ddd",
+      score: 10,
+    },
+    student2: {
+      firstName: "aaa",
+      lastName: "aaaa",
+      phoneNumber: "111",
+      address: "ddd",
+      score: 10,
+    },
+    student3: {
+      firstName: "aaa",
+      lastName: "aaaa",
+      phoneNumber: "111",
+      address: "ddd",
+      score: 10,
+    },
   },
   classB: {
-    student1: {},
-    student2: {},
-    student3: {},
+    student1: {
+      firstName: "aaa",
+      lastName: "aaaa",
+      phoneNumber: "111",
+      address: "ddd",
+      score: 10,
+    },
+    student2: {
+      firstName: "aaa",
+      lastName: "aaaa",
+      phoneNumber: "111",
+      address: "ddd",
+      score: 10,
+    },
+    student3: {
+      firstName: "aaa",
+      lastName: "aaaa",
+      phoneNumber: "111",
+      address: "ddd",
+      score: 10,
+    },
   },
 };
