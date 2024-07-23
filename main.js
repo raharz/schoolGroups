170f342c82a3cc30a7bae5bb298b97b88d33e146
raha <raharezaaeii@gmail.com>
2024-07-23 16:09:59 +0330

    <fix>[fix array]
    fix arrays for classes and students

--- main.js
+++ main.js
@@ -1,51 +1,65 @@
 let school = {
-  classA: {
-    student1: {
-      firstName: "raha",
-      lastName: "rezaei",
-      phoneNumber: "09398877307",
-      address: "sangi",
-      score: 20,
+  classes: [
+    // start classA
+    // students of classA
+    {
+      students: [
+        {
+          firstName: "raha",
+          lastName: "rezaei",
+          phoneNumber: "09398877307",
+          address: "sangi",
+          score: 20,
+        },
+
+        {
+          firstName: "hasti",
+          lastName: "ahmadi",
+          phoneNumber: "09307768976",
+          address: "modares",
+          score: 17,
+        },
+        {
+          firstName: "hanie",
+          lastName: "danesh",
+          phoneNumber: "09176665665",
+          address: "bahmani",
+          score: 18,
+        },
+      ],
     },
-    student2: {
-      firstName: "hasti",
-      lastName: "ahmadi",
-      phoneNumber: "09307768976",
-      address: "modares",
-      score: 17,
+    // finish classA
+
+    // start classB
+    // students of classB
+    {
+      students: [
+        {
+          firstName: "sogand",
+          lastName: "gharibi",
+          phoneNumber: "09306654334",
+          address: "bahonar",
+          score: 20,
+        },
+        {
+          firstName: "sara",
+          lastName: "razavi",
+          phoneNumber: "09904433223",
+          address: "sangi",
+          score: 10,
+        },
+        {
+          firstName: "ava",
+          lastName: "ebrahimi",
+          phoneNumber: "09307788998",
+          address: "saheli",
+          score: 16,
+        },
+      ],
+      // finish classB
     },
-    student3: {
-      firstName: "hanie",
-      lastName: "danesh",
-      phoneNumber: "09176665665",
-      address: "bahmani",
-      score: 18,
-    },
-  },
-  classB: {
-    student1: {
-      firstName: "sogand",
-      lastName: "gharibi",
-      phoneNumber: "09306654334",
-      address: "bahonar",
-      score: 20,
-    },
-    student2: {
-      firstName: "sara",
-      lastName: "razavi",
-      phoneNumber: "09904433223",
-      address: "sangi",
-      score: 10,
-    },
-    student3: {
-      firstName: "ava",
-      lastName: "ebrahimi",
-      phoneNumber: "09307788998",
-      address: "saheli",
-      score: 16,
-    },
-  },
+  ],
 };
 
-console.log(school);
-console.log(school.classA.student1);
+// console.log(school);
+// console.log(school.classA.student1);
